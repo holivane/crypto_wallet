e9a29a33b918d1c1ebebf388b8cc57997c08b7a5
holivane <holivane@gmail.com>
2024-08-05 08:57:44 -0300

    Add helper rails_environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
   def date_br(date_us)
     date_us.strftime('%d/%m/%Y')
   end
+
+  def rails_environment
+    if Rails.env.development?
+      'Desenvolvimento'
+    elsif Rails.env.production?
+      'Produção'
+    else
+      'Teste'
+    end
+  end
 end
