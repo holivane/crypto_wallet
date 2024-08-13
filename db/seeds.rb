3cd7cfae7134c7ba0d964491513051eab30a9dbe
holivane <holivane@gmail.com>
2024-08-13 15:46:43 -0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating Coins...'
+
+Coin.create!(
+  description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://cryptologos.cc/logos/bitcoin-sv-bsv-logo.png'
+)
+
+Coin.create!(
+  description: 'Ethereum',
+  acronym: 'ETH',
+  url_image: 'https://cryptologos.cc/logos/ethereum-eth-logo.png'
+)
+
+Coin.create!(
+  description: 'Dash',
+  acronym: 'DASH',
+  url_image: 'https://cryptologos.cc/logos/dash-dash-logo.png'
+)
+
+Coin.create!(
+  description: 'Solana',
+  acronym: 'SOL',
+  url_image: 'https://cryptologos.cc/logos/solana-sol-logo.png'
+)
+
+Coin.create!(
+  description: 'Near',
+  acronym: 'NEAR',
+  url_image: 'https://cryptologos.cc/logos/near-protocol-near-logo.png'
+)
+
+puts 'Coins created successfully!'
