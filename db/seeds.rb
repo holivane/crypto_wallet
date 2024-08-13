5d74e2616763f2c7b713bd5667cbb836d58271b4
holivane <holivane@gmail.com>
2024-08-13 18:36:57 -0300

    Refact Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,36 @@
 
 puts 'Creating Coins...'
 
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'https://cryptologos.cc/logos/bitcoin-sv-bsv-logo.png'
-)
+Coin.create!([
+               {
+                 description: 'Bitcoin',
+                 acronym: 'BTC',
+                 url_image: 'https://cryptologos.cc/logos/bitcoin-sv-bsv-logo.png'
+               },
 
-Coin.create!(
-  description: 'Ethereum',
-  acronym: 'ETH',
-  url_image: 'https://cryptologos.cc/logos/ethereum-eth-logo.png'
-)
+               {
+                 description: 'Ethereum',
+                 acronym: 'ETH',
+                 url_image: 'https://cryptologos.cc/logos/ethereum-eth-logo.png'
+               },
 
-Coin.create!(
-  description: 'Dash',
-  acronym: 'DASH',
-  url_image: 'https://cryptologos.cc/logos/dash-dash-logo.png'
-)
+               {
+                 description: 'Dash',
+                 acronym: 'DASH',
+                 url_image: 'https://cryptologos.cc/logos/dash-dash-logo.png'
+               },
 
-Coin.create!(
-  description: 'Solana',
-  acronym: 'SOL',
-  url_image: 'https://cryptologos.cc/logos/solana-sol-logo.png'
-)
+               {
+                 description: 'Solana',
+                 acronym: 'SOL',
+                 url_image: 'https://cryptologos.cc/logos/solana-sol-logo.png'
+               },
 
-Coin.create!(
-  description: 'Near',
-  acronym: 'NEAR',
-  url_image: 'https://cryptologos.cc/logos/near-protocol-near-logo.png'
-)
+               {
+                 description: 'Near',
+                 acronym: 'NEAR',
+                 url_image: 'https://cryptologos.cc/logos/near-protocol-near-logo.png'
+               }
+             ])
 
 puts 'Coins created successfully!'
